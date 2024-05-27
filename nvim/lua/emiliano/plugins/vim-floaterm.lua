ebfedc0a2c7d060a8cd338a4b28489bfbda9fcba
Emiliano Tisato <emilianotisato@gmail.com>
2024-05-27 16:11:23 -0600

    Added floating terminal

---
+++ nvim/lua/emiliano/plugins/vim-floaterm.lua
@@ -0,0 +1,6 @@
+return { "voldikss/vim-floaterm",
+        config = function()
+            vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+            vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+        end
+}
